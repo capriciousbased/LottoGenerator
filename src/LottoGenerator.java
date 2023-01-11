4942058a302313bd656bb014daacf625c497b0ad
capriciousbased <mdanisulhoquekhan@gmail.com>
2023-01-11 10:45:20 +0100

    Add Random Supper Number

--- src/LottoGenerator.java
+++ src/LottoGenerator.java
@@ -3,17 +3,49 @@ import java.util.Set;
 import java.util.HashSet;
 
 public class LottoGenerator {
-    public static int[] generateNumber() {
+
+    // generate Random six Numbers
+
+    public static int[] generateNumbers() {
         Random rand = new Random();
         Set<Integer> uniqueNumbers = new HashSet<>();
         while(uniqueNumbers.size()<6){
-            uniqueNumbers.add(rand.nextInt(6)+1);
+            uniqueNumbers.add(rand.nextInt(49)+1);
         }
 
         return uniqueNumbers.stream().mapToInt(i->i).toArray();
         
     }
-    public static void main(String[] args) throws Exception {
 
+    // generate Random Two Numbers
+
+    public static int[] generateSupperNumbers() {
+        Random rand = new Random();
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        while(uniqueNumbers.size()<2){
+            uniqueNumbers.add(rand.nextInt(9)+1);
+        }
+
+        return uniqueNumbers.stream().mapToInt(i->i).toArray();
+        
+    }
+    public static void main(String[] args) {
+
+        int[] lottoNumbers = LottoGenerator.generateNumbers();
+        int[] supperNumbers = LottoGenerator.generateSupperNumbers();
+      
+    // Print six lotto Number : 
+
+        System.out.println("Six out of fortynine: ");
+        for (int i = 0; i < lottoNumbers.length; i++) {
+            System.out.print(lottoNumbers[i] + " ");
+        }
+
+    // Print Two lotto Supper Number :
+     
+        System.out.println("Super Number: Two out of Ten: ");
+        for (int j = 0; j < supperNumbers.length; j++) {
+            System.out.print(supperNumbers[j] + " ");
+        }
     }
 }
