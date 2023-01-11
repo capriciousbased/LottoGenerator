9d89895154e9f20af2289320e193620598d8088b
capriciousbased <mdanisulhoquekhan@gmail.com>
2023-01-11 10:11:16 +0100

    generateNumber mehtod added

--- src/LottoGenerator.java
+++ src/LottoGenerator.java
@@ -1,5 +1,19 @@
+import java.util.Random;
+import java.util.Set;
+import java.util.HashSet;
+
 public class LottoGenerator {
+    public static int[] generateNumber() {
+        Random rand = new Random();
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        while(uniqueNumbers.size()<6){
+            uniqueNumbers.add(rand.nextInt(6)+1);
+        }
+
+        return uniqueNumbers.stream().mapToInt(i->i).toArray();
+        
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Lotto Generatro!");
+
     }
 }
